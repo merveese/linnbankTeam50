a67949d1b94fd5da5f24a820239d8bc02eb343c0
Ali Atilgan <a.atilgan1720@gmail.com>
2021-08-18 18:20:39 -0500

    1

--- src/test/java/linnbank/stepdefinitions/SampleStepdefs.java
+++ src/test/java/linnbank/stepdefinitions/SampleStepdefs.java
@@ -9,6 +9,7 @@ public class SampleStepdefs {
     @Given("user on home page")
     public void user_on_home_page() {
         System.out.println("Hello WORLD");
+        //test
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
 
     }
