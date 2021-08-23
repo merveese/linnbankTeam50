ba3bb6b928553ffe54970bcbc981dc80cd258c37
merveese <79477320+merveese@users.noreply.github.com>
2021-08-23 00:20:37 -0500

    us_9_10

---
+++ src/test/java/linnbank/pages/Gmi_CreateACustomerPage.java
@@ -0,0 +1,39 @@
+package linnbank.pages;
+
+import linnbank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Gmi_CreateACustomerPage {
+    public Gmi_CreateACustomerPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy (id="search-ssn")
+    public WebElement ssnBox;
+    @FindBy (xpath = "//*[contains(text(),'Search')]")
+    public WebElement searchButton;
+    @FindBy (id="tp-customer-firstName")
+    public WebElement firstNameBox;
+    @FindBy (id="tp-customer-lastName")
+    public WebElement lastNameBox;
+    @FindBy (id="tp-customer-middleInitial")
+    public WebElement middleInitialBox;
+    @FindBy (id="tp-customer-email")
+    public WebElement emailBox;
+    @FindBy (id="tp-customer-mobilePhoneNumber")
+    public WebElement mobileNumber;
+    @FindBy (id="tp-customer-phoneNumber")
+    public WebElement phoneNumber;
+    @FindBy (id="tp-customer-zipCode")
+    public WebElement zipCode;
+    @FindBy (id="tp-customer-address")
+    public WebElement address;
+    @FindBy (id="tp-customer-city")
+    public WebElement city;
+    @FindBy (id="tp-customer-country")
+    public WebElement country;
+    @FindBy (id="tp-customer-state")
+    public WebElement state;
+
+}
