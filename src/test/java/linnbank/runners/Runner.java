3b800961df76b989cadc90de7cfb7e966f57b579
Ali Atilgan <a.atilgan1720@gmail.com>
2021-08-23 23:45:37 -0500

    1

--- src/test/java/linnbank/runners/Runner.java
+++ src/test/java/linnbank/runners/Runner.java
@@ -5,12 +5,15 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin={"html:target/default-cucumber-reports.html",
+        plugin={
+                "html:target/default-cucumber-reports.html",
                 "json:target/json-reports/cucumber.json",
-                "junit:target/xml-report/cucumber.xml"},
+                "junit:target/xml-report/cucumber.xml"
+        },
         features="src/test/resources/features",
         glue= "linnbank/stepdefinitions",
-        tags = "@sample",
+
+        tags = "@testing",
         dryRun = false
 )
 
