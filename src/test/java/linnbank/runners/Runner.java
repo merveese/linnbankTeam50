9049ffb6113c82072fd72ac38e702b4db2bae87d
Sibel Atilgan <s.atilgan1993@gmail.com>
2021-08-25 00:58:28 -0500

    first

--- src/test/java/linnbank/runners/Runner.java
+++ src/test/java/linnbank/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features="src/test/resources/features",
         glue= "linnbank/stepdefinitions",
 
-        tags = "@testing",
+        tags = "@invalid_registration",
         dryRun = false
 )
 
